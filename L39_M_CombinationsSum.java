73ff85b17823e09569c27d78b365aa5d063e3fa0
kikihayashi <r05522504@g.ntu.edu.tw>
2022-08-27 17:46:46 +0800

    更新

--- L39_M_CombinationsSum.java
+++ L39_M_CombinationsSum.java
@@ -1,42 +1,50 @@
 package Leetcode;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * https://leetcode.com/problems/combinations/
+ * https://leetcode.com/problems/combination-sum/
  */
 
-public class L77_M_Combinations {
+public class L39_M_CombinationsSum {
 
     private static List<List<Integer>> result = new LinkedList<>();
 
     public static void main(String[] args) {
-        combine(6, 3);
+        int[] candidates = new int[]{2, 3, 5};
+        int target = 8;
+        combinationSum(candidates, target);
+//        System.out.println(8%2);
         System.out.println(result);
     }
 
-    public static List<List<Integer>> combine(int n, int k) {
-        backtrackingMy(1, n, k, new LinkedList<>());
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        backtracking(0, 0, candidates, target, new LinkedList<Integer>());
         return result;
     }
 
-    //別人寫的：2 ms、45.3 MB
-    public static void backtracking(int start, int n, int k, LinkedList<Integer> list) {
-        if (list.size() == k) {
+    public static void backtracking(int start, int now, int[] candidates, int target, LinkedList<Integer> list) {
+        if (now == target) {
             result.add(new LinkedList<>(list));
             return;
+        } else if (now > target) {
+            now -= candidates[start + 1];
+            list.removeLast();
         }
 
-        for (int i = start; i <= n - (k - list.size()) + 1; i++) {
-            list.add(i);
-            backtracking(i + 1, n, k, list);
-            list.remove(list.size() - 1);
+        for (int i = start; i < candidates.length; i++) {
+            list.add(candidates[i]);
+            now += candidates[i];
+            backtracking(start, now, candidates, target, list);
+            list.removeLast();
         }
+        list.removeLast();
+        backtracking(start + 1, now, candidates, target, list);
+
     }
 
-    //81 ms、65.4 MB
     public static void backtrackingMy(int start, int n, int k, LinkedList<Integer> list) {
         if (list.size() == k) {
             result.add(new LinkedList<>(list));
